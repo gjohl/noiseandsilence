1a8d278c70cc98e73088aedcf2441eb0d9eeed13
Gurpreet Johl <gurpreetjohl@gmail.com>
2019-02-09 16:28:43 +0000

    adding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='noiseandsilence',
+    version='0.1.0',
+    author='Gurpreet Johl',
+    author_email='gurpreetjohl.com',
+    packages=['noiseandsilence'],
+    description='noiseandsilnce research submission.',
+    long_description=open('README.md').read(),
+)
